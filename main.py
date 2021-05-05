8a8547313b39940f8c9d219d53af65fa2f24c6bf
OvOlzhas <turlybekov.o@phystech.edu>
2021-05-05 03:17:38 +0300

    Created accept.txt & useragent.txt. Added comments in code.

--- main.py
+++ main.py
@@ -1,44 +1,52 @@
 import re
+import requests
+import sqlite3
 import sys
 import telebot
-import requests
+
 from bs4 import BeautifulSoup
-import sqlite3
 
 bot = telebot.TeleBot('1694007852:AAH_S25nSS6Fo4ChnGaMVK5VSKZjiifr2eo')
 HOST = 'https://en.wikipedia.org/wiki/'
 HEADERS = {
-    'accept': 
-    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36'
+    'accept': f'{open("accept.txt").read()}',
+    'user-agent': f'{open("useragent.txt").read()}'
 }
-musor = ['в', 'без', 'до', 'из', 'к', 'на', 'по', 'о', 'от', 'перед', 'при', 'через', 'с', 'у',
-         'за', 'над', 'об', 'под', 'про', 'для', 'и', 'не', 'что', 'как', 'а', 'то',
-         'of', 'the', 'in', 'to', 'a', 'an', 'from', 'into', 'and', 'on', 'at', 'for', 'as', 'by',
-         'is', 'are', '']
+trash = ['of', 'the', 'in', 'to', 'a', 'an', 'from', 'into', 'and', 'on',
+         'at', 'for', 'as', 'by', 'is', 'are', '']
 
 
 def get_html(url, message, params=''):
+    '''
+    Возвращает html по url.
+    '''
     if not HOST in url:
+        # Нет строки HOST в url
         bot.send_message(message.from_user.id, "Ссылка не корректна.")
         sys.exit()
     try:
         return requests.get(url, headers=HEADERS, params=params)
     except:
+        # Открытие сайта не увенчалось успехом
         bot.send_message(message.from_user.id, "Ссылка не корректна.")
         sys.exit()
 
 
 def get_content(html):
+    '''
+    Возвращает текст из Википедии(TEXT), а также заголовок(NAME).
+    '''
     TEXT = ""
     soup = BeautifulSoup(html, 'lxml')
     body = soup.find("div", class_="mw-parser-output")
     NAME = soup.find("h1", id="firstHeading").get_text()
     texts_p = body.find_all('p')
     texts_span = body.find_all('span', class_="mw-headline")
+    # Удаляются table из html
     for table in body.find_all('table'):
         table.clear()
 
+    # Добавляются в текст "p" и "span"
     for text in body.find_all(['p', 'span']):
         if text in texts_p or text in texts_span:
             TEXT += text.get_text() + '\n'
@@ -46,9 +54,14 @@ def get_content(html):
 
 
 def create_table(cur, conn):
+    '''
+    Создается бд с sqlite. При наличии - бд пересоздается.
+    '''
+    # Опусташается бд
     cur.execute('DROP TABLE IF EXISTS user')
     cur.execute('DROP TABLE IF EXISTS wiki')
 
+    # Создается бд
     cur.execute('''
         CREATE TABLE user (
             user_id INTEGER,
@@ -66,11 +79,18 @@ def create_table(cur, conn):
 
 
 def add_user(cur, conn, user_id, user_name):
+    '''
+    Добавляется новый пользователь в таблицу user.
+    '''
     cur.execute(f'''INSERT INTO user (user_id, user_name, last_text_name, last_url, top) 
                     VALUES ('{user_id}', '{user_name}', '', '', '')''')
     conn.commit()
 
 def check_user(cur, conn, user_id, user_name):
+    '''
+    Проверка на наличие пользователся в таблице user.
+    При отсутствии добавляется.
+    '''
     cur.execute(f'''SELECT *
                     FROM user
                     WHERE user_id={user_id}''')
@@ -78,6 +98,9 @@ def check_user(cur, conn, user_id, user_name):
         add_user(cur, conn, user_id, user_name)
 
 def get_user_content(cur, conn, message):
+    '''
+    Возвращаются last_text_name, last_url и top пользователья из таблицы user.
+    '''
     cur.execute(f'''SELECT last_text_name, last_url, top
                     FROM user
                     WHERE user_id={message.from_user.id}''')
@@ -87,11 +110,17 @@ def get_user_content(cur, conn, message):
     return row
 
 def add_wiki(cur, conn, NAME):
+    '''
+    Добавляется новая статья из Википедии в таблицу wiki.
+    '''
     cur.execute(f'''INSERT INTO wiki (name, count) 
                     VALUES ('{NAME}', '1')''')
     conn.commit()
 
 def get_top_wiki(cur, conn, message):
+    '''
+    Возвращается 5 наибольее часто запрашиваемых статей из Википедии.
+    '''
     cur.execute(f'''SELECT count, name
                     FROM wiki''')
     WTOP = ""
@@ -107,17 +136,22 @@ def get_top_wiki(cur, conn, message):
 
 
 def save_content(user_id, user_name, url, NAME, TOP):
+    '''
+    Обновляется статья в таблице wiki, а также пользователь в user.
+    '''
     conn = sqlite3.connect('database.db')
     cur = conn.cursor()
     check_user(cur, conn, user_id, user_name)
+    # Обновляется пользователь
     cur.execute(f'''UPDATE user 
                     SET last_text_name="{NAME}", top="{TOP}", last_url="{url}"
                     WHERE user_id={user_id}''')
-    
+
     cur.execute(f'''SELECT count, name
                     FROM wiki
                     WHERE name="{NAME}"''')
     row = cur.fetchall()
+    # Обновляется статья или же создается
     if len(row) == 0:
         add_wiki(cur, conn, NAME)
     else:
@@ -129,19 +163,27 @@ def save_content(user_id, user_name, url, NAME, TOP):
 
 
 def top_words(TEXT):
-    global musor
+    '''
+    Возвращается 5 наибольее часто встречающиеся слова, 
+    кроме слов из trash.
+    '''
+    global trash
 
     WORDS = {}
     for word in TEXT.split():
         word = word.lower()
         copy = word
+        # Удаляются нежелаемые символы
         for c in copy:
             if ord('a') > ord(c) or ord(c) > ord('z'):
                 word = re.sub(f'[{c}]', '', word)
-        if not word.lower() in musor:
+        # Проверка на содержании в trash
+        if not word.lower() in trash:
+            # Добавляется слово
             WORDS[word.lower()] = WORDS.get(word, 0) + 1
     words = []
     TOP = ""
+    # Берутся слова, которые встречаются наибольшее количество раз
     for word in WORDS.items():
         words.append((word[1], word[0]))
     for (i, word) in zip(range(5), reversed(sorted(words))):
@@ -152,11 +194,17 @@ def top_words(TEXT):
 
 
 def print_text(message, NAME, TEXT):
+    '''
+    Отправляется сообщение с первым абзацем текста с кнопками
+    "View all text" и "Top 5 words".
+    '''
+    # Создание кнопок
     markup = telebot.types.InlineKeyboardMarkup()
     button1 = telebot.types.InlineKeyboardButton("View all text", callback_data="AllText")
     button2 = telebot.types.InlineKeyboardButton("Top 5 words", callback_data="TopWords")
     markup.row(button1, button2)
     
+    # Берется первый абзац
     start = 0
     first_text = ""
     for start in range(0, len(TEXT)):
@@ -166,7 +214,9 @@ def print_text(message, NAME, TEXT):
             continue
         first_text += TEXT[start]
     
+    # Удаляется первый абзац
     TEXT = TEXT[start:len(TEXT)]
+    # Отправка первого абзаца
     if (len(TEXT) != 0):
         bot.send_message(message.chat.id, "*" + NAME + "*\n\n" + first_text, reply_markup=markup, parse_mode='Markdown')
     else:
@@ -174,6 +224,9 @@ def print_text(message, NAME, TEXT):
 
 
 def print_top(message):
+    '''
+    Отправляется сообщение с 5 наиболее встречаемыми словами.
+    '''
     conn = sqlite3.connect('database.db')
     cur = conn.cursor()
     (NAME, url, TOP) = get_user_content(cur, conn, message)
@@ -184,13 +237,18 @@ def print_top(message):
 
 @bot.callback_query_handler(lambda q: q.data == 'AllText')
 def callback_query(call):
+    '''
+    Нажата кнопка "View all text".
+    Отправляются сообщения с текстом без первого абзаца.
+    '''
     conn = sqlite3.connect('database.db')
     cur = conn.cursor()
     (NAME, url, TOP) = get_user_content(cur, conn, call)
     
     html = get_html(url, call)
     NAME, TEXT = get_content(html.text)
-
+    
+    # Удаляется первый абзац
     ok = False
     for start in range(0, len(TEXT)):
         if TEXT[start] == '\n':
@@ -199,20 +257,29 @@ def callback_query(call):
             continue
         ok = True
     TEXT = TEXT[start:len(TEXT)]
+
+    # Отправка текста без первого абзаца
     bot.answer_callback_query(call.id)
-    for x in range(0, len(TEXT), 4096):
+    for x in range(0, len(TEXT), 4096): # 4096 - макс. количество символов в одном сообщение
         bot.send_message(call.from_user.id, TEXT[x:x+4096])
     conn.close()
 
 
 @bot.callback_query_handler(lambda q: q.data == 'TopWords')
 def callback_query(call):
+    '''
+    Нажата кнопка "Top 5 words".
+    Отправляется сообщение с 5 наиболее встречаемыми словами.
+    '''
     bot.answer_callback_query(call.id)
     print_top(call)
 
 
 @bot.message_handler(commands=["start", "help"])
 def get_command(message):
+    '''
+    Обрабатываются команды /start и /help.
+    '''
     if message.text == "/help" or message.text == "/start":
         mess = '''
 Доступные тебе команды:    
@@ -233,17 +300,22 @@ _(Cсылка должна начинаться как https://en.wikipedia.org/
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    global TOP
+    '''
+    Ожидаются команды "GetText", "LastTop", "LastWiki" и "TopWiki".
+    '''
     words = message.text.split()
     if words[0].lower() == "gettext" and len(words) != 1:
+        # Введено GetText с url
         html = get_html(words[1], message)
         NAME, TEXT = get_content(html.text)
         TOP = top_words(TEXT)
         print_text(message, NAME, TEXT)
         save_content(message.from_user.id, message.from_user.first_name, words[1], NAME, TOP)
     elif words[0].lower() == "lasttop":
+        # Введено LastTop
         print_top(message)
     elif words[0].lower() == "lastwiki":
+        # Введено LastWiki
         conn = sqlite3.connect('database.db')
         cur = conn.cursor()
         (NAME, url, TOP) = get_user_content(cur, conn, message)
@@ -251,15 +323,20 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, NAME)
         conn.close()
     elif words[0].lower() == "topwiki":
+        # Введено TopWiki
         conn = sqlite3.connect('database.db')
         cur = conn.cursor()
         bot.send_message(message.from_user.id, get_top_wiki(cur, conn, message))
         conn.close()
     else:
+        # Неизвестная команда
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
 
 def start():
+    '''
+    Создается таблица.
+    '''
     conn = sqlite3.connect('database.db')
     cur = conn.cursor()
     create_table(cur, conn)
@@ -267,4 +344,5 @@ def start():
 
 start()
 
+# Бот работает бесконечно
 bot.polling(none_stop=True, interval=0)
