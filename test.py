76a8b7baf4a5ee29339735cbf864d3d279cb4332
OvOlzhas <turlybekov.o@phystech.edu>
2021-05-06 18:36:54 +0300

    Fixed flake8

--- test.py
+++ test.py
@@ -30,13 +30,13 @@ class TestMain(unittest.TestCase):
         main.add_user(self.cur, self.conn, 123, 'Ippo')
         self.cur.execute('''SELECT user_id, user_name, last_text_name, last_url, top
                         FROM user''')
-        self.assertEqual(self.cur.fetchall(), [(123, 'Ippo', '', '', '')])  
+        self.assertEqual(self.cur.fetchall(), [(123, 'Ippo', '', '', '')]) 
 
     def test_check_user(self):
         main.check_user(self.cur, self.conn, 123, 'Ippo')
         self.cur.execute('''SELECT user_id, user_name, last_text_name, last_url, top
                         FROM user''')
-        self.assertEqual(self.cur.fetchall(), [(123, 'Ippo', '', '', '')]) 
+        self.assertEqual(self.cur.fetchall(), [(123, 'Ippo', '', '', '')])
 
     def test_get_user_content(self):
         main.check_user(self.cur, self.conn, 123, 'Ippo')    
@@ -46,7 +46,7 @@ class TestMain(unittest.TestCase):
         main.add_wiki(self.cur, self.conn, 'Hajime no Ippo')
         self.cur.execute('''SELECT name, count
                         FROM wiki''')
-        self.assertEqual(self.cur.fetchall(), [('Hajime no Ippo', 1)])  
+        self.assertEqual(self.cur.fetchall(), [('Hajime no Ippo', 1)])
 
     def test_get_top_wiki(self):
         main.add_wiki(self.cur, self.conn, 'a')
@@ -58,8 +58,7 @@ class TestMain(unittest.TestCase):
         self.cur.execute('''SELECT user_id, user_name, last_text_name, last_url, top
                         FROM user
                         WHERE user_id=123''')
-        self.assertEqual(self.cur.fetchone(), (123, 'Ippo', 
-        'Hajime no Ippo', 'url', '1. b => 1\n2. a => 1\n'))
+        self.assertEqual(self.cur.fetchone(), (123, 'Ippo', 'Hajime no Ippo', 'url', '1. b => 1\n2. a => 1\n'))
         self.cur.execute('''SELECT name, count
                         FROM wiki
                         WHERE name="Hajime no Ippo"''')
